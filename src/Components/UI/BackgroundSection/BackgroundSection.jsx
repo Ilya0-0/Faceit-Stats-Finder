ecbfa2d242b7f6b3d84f263650d2f2941d6bef42
Ilya Butov <ilya-butov-00@mail.ru>
2025-02-04 14:25:02 +0300

    feat: Fixed logic in backgroundSection-component

--- src/Components/UI/BackgroundSection/BackgroundSection.jsx
+++ src/Components/UI/BackgroundSection/BackgroundSection.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const BackgroundSection = ({ className, children }) => {
-  return <section className={className}>{children}</section>;
+const BackgroundSection = ({ image, className, children }) => {
+  return (
+    <section style={{ backgroundImage: `url(${image})` }} className={className}>
+      {children}
+    </section>
+  );
 };
 
 export default BackgroundSection;
