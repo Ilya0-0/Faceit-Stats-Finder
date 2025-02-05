90b289dc60057818a87f90f09ebb45cb1b413ca6
Ilya Butov <ilya-butov-00@mail.ru>
2025-02-05 15:38:12 +0300

    feat: add PlaceholderContent component

---
+++ src/Components/UI/PlaceholderContent/PlaceholderContent.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./placeholderContent.module.scss";
+
+const PlaceholderContent = ({ title }) => {
+  return (
+    <div className={styles.placeholderContent}>
+      {title && <p className={styles.placeholderContent_title}>{title}</p>}
+      <svg
+        className={styles.placeholderContent_img}
+        viewBox="0 0 100 100"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M0 100L5.71335 10.4492L42.9811 6.24476L0 100Z"
+          fill="#FF5500"
+        />
+        <path
+          d="M40.4688 28.0499L50.8683 5.63689L100 0L40.4688 28.0499Z"
+          fill="#FF5500"
+        />
+        <path
+          d="M30.0692 50.4629L37.8689 33.6532L71.7517 29.7836L30.0692 50.4629Z"
+          fill="white"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default PlaceholderContent;
